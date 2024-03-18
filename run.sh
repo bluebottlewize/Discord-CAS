45b1a131c2e1df34a7cc72ffc7d3d7b647068fb7
Ankith <itsankith26@gmail.com>
2024-03-19 00:36:20 +0530

    Add CASBOT_DIR handling and chmod +x sh files

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-docker-compose -p cas up --build
+
+if [[ $CASBOT_DIR == "" ]]; then
+    CASBOT_DIR=.
+fi
+
+docker-compose -f $CASBOT_DIR/docker-compose.yml -p cas up --build
--- stop.sh
+++ stop.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-docker-compose -p cas down
+
+if [[ $CASBOT_DIR == "" ]]; then
+    CASBOT_DIR=.
+fi
+
+docker-compose -f $CASBOT_DIR/docker-compose.yml -p cas down
