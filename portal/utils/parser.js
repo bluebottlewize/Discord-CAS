c0808230fbaac1e5b5f82824b58a36ffcf883a43
Samyak Mishra <doo.di.diddle.doo@gmail.com>
2024-03-26 17:18:12 +0530

    Lint utils/parser.js
    Added parentheses around an assignment in a conditional
    expression.

--- portal/utils/parser.js
+++ portal/utils/parser.js
@@ -10,7 +10,7 @@ function parse(string) {
   var match;
 
   for(let i = 0, len = lines.length; i !== len; i++){
-    if(match = lines[i].match(REG_GROUP))
+    if((match = lines[i].match(REG_GROUP)))
       object[match[1]] = group = object[match[1]] || {};
     else if(group && (match = lines[i].match(REG_PROP)))
       group[match[1]] = match[2];
