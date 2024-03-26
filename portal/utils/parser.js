d5b05d4a616b452b4d22d233c43f524803d6e2ae
Samyak Mishra <doo.di.diddle.doo@gmail.com>
2024-03-26 16:28:21 +0530

    Autofix most linting issues in utils/parser.js

--- portal/utils/parser.js
+++ portal/utils/parser.js
@@ -1,33 +1,33 @@
-const fs = require('fs')
+const fs = require("fs");
 
-const REG_GROUP = /^\s*\[(.+?)\]\s*$/
-const REG_PROP = /^\s*([^#].*?)\s*=\s*(.*?)\s*$/
+const REG_GROUP = /^\s*\[(.+?)\]\s*$/;
+const REG_PROP = /^\s*([^#].*?)\s*=\s*(.*?)\s*$/;
 
 function parse(string) {
-	var object = {}
-	var lines = string.split('\n')
-	var group
-	var match
+  var object = {};
+  var lines = string.split("\n");
+  var group;
+  var match;
 
-	for(let i = 0, len = lines.length; i !== len; i++){
-		if(match = lines[i].match(REG_GROUP))
-			object[match[1]] = group = object[match[1]] || {};
-		else if(group && (match = lines[i].match(REG_PROP)))
-			group[match[1]] = match[2];
-	}
+  for(let i = 0, len = lines.length; i !== len; i++){
+    if(match = lines[i].match(REG_GROUP))
+      object[match[1]] = group = object[match[1]] || {};
+    else if(group && (match = lines[i].match(REG_PROP)))
+      group[match[1]] = match[2];
+  }
 
-	return object;
+  return object;
 }
 
 function parseFile(file, callback){
-	fs.readFile(file, 'utf-8', function(error, data){
-		if(error)
-			return callback(error);
+  fs.readFile(file, "utf-8", function(error, data){
+    if(error)
+      return callback(error);
 
-		callback(null, parse(data))
-	})
+    callback(null, parse(data));
+  });
 }
 
 module.exports = {
   parseFile,
-}
+};
