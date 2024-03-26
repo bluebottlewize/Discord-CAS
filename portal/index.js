b1a2ae51722c75fbbc19da1dd7074bd732a5808b
Samyak Mishra <doo.di.diddle.doo@gmail.com>
2024-03-26 16:22:47 +0530

    Auto fix linting issues in index.js

--- portal/index.js
+++ portal/index.js
@@ -1,14 +1,14 @@
-const config = require('./utils/config')
-const app = require('./app')
-const logger = require('./utils/logger')
-const mongoose = require('mongoose');
-require('dotenv').config()
+const config = require("./utils/config");
+const app = require("./app");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 mongoose.connect( config.ATLAS_URL,
   { useNewUrlParser: true, useUnifiedTopology: true }, () => { 
-    logger.info('Database connected successfully.');
-});
+    logger.info("Database connected successfully.");
+  });
 
 app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
+  logger.info(`Server running on port ${config.PORT}`);
 });
